9782b4901a9c31910da073f71d0af868b562d966
Rickelmy <rickrickelmy98@gmail.com>
2025-04-10 16:36:05 -0300

    feat: estruturação + configurações iniciais

---
+++ dashtask-ui/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-root',
+    template: '<router-outlet></router-outlet>',
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    imports: [RouterOutlet],
+})
+export class AppComponent {
+    title = 'dashtask-ui';
+}
---
+++ dashtask-ui/src/main.ts
@@ -0,0 +1,12 @@
+import { provideZoneChangeDetection } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
+}).catch((err) => console.error(err));
